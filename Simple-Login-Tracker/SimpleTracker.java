f2b363b9c9a88919bca35c760570163cb8dc61e4
Kevin Bai <84043195+Pwumpkin@users.noreply.github.com>
2021-05-30 21:35:51 -0700

    date object

--- Simple-Login-Tracker/SimpleTracker.java
+++ Simple-Login-Tracker/SimpleTracker.java
@@ -4,12 +4,15 @@
  * Takes curent time from system
  * 
  * Callum 
- * Version 1.0
+ * Version 1.1
+     * formatted output string to
  */
 
 
 // Standard import for the Scanner class
-import java.util.*;
+import java.util.Scanner;
+import java.util.Date;
+
 
 import java.io.*;
 
@@ -24,28 +27,27 @@ public class SimpleTracker {
         FileWriter fw = new FileWriter(file,true);// apenend to file true
         PrintWriter pw = new PrintWriter(fw);
         
+        Date date = new Date();
+        
         
         // Current milliseconds since UNIX epoch Jan 1 1970
         long totalMilliseconds = System.currentTimeMillis();
-        
         // total seconds
         long totalSecond = totalMilliseconds / 1000;
-        
         long currentSecond = (int) (totalSecond % 60);
-        
         //get total miniutes
         long totalMinute = totalSecond / 60;
-     
         long currentMinute = (int) (totalMinute % 60);
-        
         //get the total hour
         long totalHour = totalMinute/ 60;
-
         long curentHour = (int) (totalHour % 24 + 6);//add +6 to convert GMT to MST
         
-        //Print the current time 
-        pw.printf("%s,%s,%s,", currentSecond,currentMinute,curentHour);
         
+                
+        
+        //Print the current time 
+        pw.printf("Current time: %s:%s:%s %n", currentSecond,currentMinute,curentHour);
+        pw.println("Current date using a Date objecti is:" + date);
         pw.println();
         
         pw.close();// close print writer
